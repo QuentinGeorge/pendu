7704db8a18e5bb7e64ae097a377ba9bf5e47c398
Quentin George <quentingeorge7@gmail.com>
2017-02-20 14:15:38 +0100

    Add controller remember tried letters, display & remove from select

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,13 +1,16 @@
 <?php
 include('settings.php');
 
-if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if(isset($_GET['letter'])) {
-        $sTriedLetters = $sTriedLetters . $_GET['letter'];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST['select_letter'])) {
+        $sTriedLetters = $_POST['letters'] . $_POST['select_letter'];
+        for($i=0; $i < strlen($sTriedLetters); $i++) {
+            $aLetters[str_split($sTriedLetters)[$i]] = false;
+        }
     }
 }
 
-if (!$bDead) {
+if(!$bDead) {
     $sView = '_form.php';
 }else {
     $sView = '_gameOver.php';
--- views/_form.php
+++ views/_form.php
@@ -3,15 +3,15 @@
 <form action="index.php" method="post">
     <fieldset>
         <legend>Il te reste <?= 'X' ?> essais pour sauver ta peau</legend>
-        <label for="letter">Choissis ta lettre&nbsp;: </label>
-        <select name="Remaining_letters">
+        <label for="letter">Choissis ta lettre</label>
+        <select id="letter" name="select_letter">
             <?php foreach ($aLetters as $key => $value): ?>
                 <?php if ($value === true): ?>
                     <option value="<?= $key; ?>"><?= $key; ?></option>
                 <?php endif; ?>
             <?php endforeach; ?>
         </select>
-        <input type="hidden" name="letter" value="<?= $sTriedLetters; ?>">
+        <input type="hidden" name="letters" value="<?= $sTriedLetters; ?>">
         <input type="submit" value="Esseyer cette lettre">
     </fieldset>
 </form>
