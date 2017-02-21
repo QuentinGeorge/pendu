3670a161e282a49398a92cfcc4b6fce6fc9ab027
Quentin George <quentingeorge7@gmail.com>
2017-02-21 15:02:26 +0100

    Update display images by game steps

--- views/_main.php
+++ views/_main.php
@@ -1,4 +1,4 @@
 <h1>Trouve le mot en moins de <?= MAX_TRIALS; ?> coups&nbsp;!</h1>
 <p>Le mot à deviner compte <?= $iWordLength; ?> lettres&nbsp;: <?= $sHiddenWord ?></p>
-<img src="./img/pendu0.jpg" alt="">
+<img src="./img/pendu<?= $iRemainingTrials; ?>.jpg" alt="">
 <p>Voici les lettres que tu as déjà essayées&nbsp;: <?= $sTriedLetters; ?></p>
