a011b60752eb929c1d5b7cc2a685970f2e37e77a
QuentinGeorge <quentingeorge7@gmail.com>
2017-02-23 17:20:10 +0100

    Update styles word bold & border-radius

--- views/_gameOver.php
+++ views/_gameOver.php
@@ -1,6 +1,6 @@
 <?php include('_main.php') ?>
 
 <div class="lost">
-    <p>Tu es mort&nbsp;! Le mot a trouver était <?= $sWord; ?></p>
+    <p>Tu es mort&nbsp;! Le mot a trouver était <span class="word"><?= $sWord; ?></span></p>
     <a href="index.php">Recommence</a>
 </div>
--- views/_gameWin.php
+++ views/_gameWin.php
@@ -1,6 +1,6 @@
 <?php include('_main.php') ?>
 
 <div class="win">
-    <p>Bravo&nbsp;! Tu semble avoir trouver le mot <?= $sWord; ?></p>
+    <p>Bravo&nbsp;! Tu semble avoir trouver le mot <span class="word"><?= $sWord; ?></span></p>
     <a href="index.php">Nouvelle partie</a>
 </div>
