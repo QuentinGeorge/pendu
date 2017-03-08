6e962f3f6ba76d95cefe14367c1da8d3cadd0e38
Quentin George <quentingeorge7@gmail.com>
2017-03-08 16:18:19 +0100

    Fix code

--- controllers/getController.php
+++ controllers/getController.php
@@ -1,7 +1,7 @@
 <?php  // ici on fait tout ce qui doit se passer a la première arrivée sur le site
 $aLetters = fGetLettersArray();
 $sSerializedLetters = fSerializeLetters($aLetters);
-$aWords = fGetFileWordsArray();;
+$aWords = fGetFileWordsArray();
 $iIndexWord = fGetWordIndex($aWords);
 $sWord = fGetWord($aWords, $iIndexWord);
 $iWordLength = strlen($sWord); // si mot contenant des accent alors utiliser mb_strlen pour préciser l'encodage car strlen comptera 2 pour un caracter accentuer
